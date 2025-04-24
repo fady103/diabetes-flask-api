b9229e54fe74ec2728125610a615264c91afb080
Fadi Beshai <77095384+fady103@users.noreply.github.com>
2025-04-24 07:55:27 +0000

    Switched to joblib model

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
-
 from flask import Flask, request, jsonify
-import pickle
+import joblib
 import numpy as np
 
 app = Flask(__name__)
 
-with open("model4.pkl", "rb") as file:
-    model = pickle.load(file)
+# تحميل النموذج باستخدام joblib
+model = joblib.load("final_model_ready.joblib")
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
-    if not data:
-        return jsonify({"error": "No input data provided"}), 400
-
     try:
         features = np.array(data["features"]).reshape(1, -1)
+        prediction = model.predict(features)
+        return jsonify({"prediction": int(prediction[0])})
     except Exception as e:
-        return jsonify({"error": str(e)}), 400
-
-    prediction = model.predict(features)
-    return jsonify({"prediction": int(prediction[0])})
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
