31104b4e50b8aad4b3754a9a1c21c424082fe13e
Fadi Beshai <77095384+fady103@users.noreply.github.com>
2025-04-24 09:52:11 +0000

    joblib

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
+
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 
 app = Flask(__name__)
 
-model = joblib.load("final_model_ready.joblib")
+model = joblib.load("model55.joblib")  
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
-    try:
-        features = np.array(data["features"]).reshape(1, -1)
-        prediction = model.predict(features)
-        return jsonify({"prediction": int(prediction[0])})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    data = request.get_json()
+    features = np.array(data["features"]).reshape(1, -1)
+    prediction = model.predict(features)
+    return jsonify({"prediction": prediction.tolist()})
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=True)
